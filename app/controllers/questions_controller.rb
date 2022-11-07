8e0285cb74e2aa6eb81d424ee553d8ec1bbf7782
Hafid Qarchi <haqarchi@gmail.com>
2022-11-07 11:41:27 +0900

    form added to the answer view

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -2,8 +2,21 @@
 
 # class for asking the coach
 class QuestionsController < ApplicationController
-  def ask; end
+  def ask
+   
+  end
 
-  def answer; end
+  def answer;
+   
+  private
+
+  def coach_answer(question)
+    if question.include? '?'
+      "Silly question, get dressed and go to work!"
+    else
+      "I don't care, get dressed and go to work!"
+    end
+  end
+  end
   # end of class QuestionsController
 end
