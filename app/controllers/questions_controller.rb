46905aee8680693777e5006ac44c2cb9cf4a4b6d
Hafid Qarchi <haqarchi@gmail.com>
2022-11-07 11:42:39 +0900

    answer logic added

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -2,21 +2,30 @@
 
 # class for asking the coach
 class QuestionsController < ApplicationController
-  def ask
-   
+  def ask; end
+
+  def answer
+    question = params[:question]
+    answer = 'I am going to work right now!'
+    coach_answer = if question == answer
+                     'Great!'
+                   elsif question == question.upcase
+                     "I can feel your motivation! #{coach_answer(question)}"
+                   else
+                     coach_answer(question)
+                   end
+    @final_answer = { 'You' => question, 'Your Coach' => coach_answer }
   end
 
-  def answer;
-   
   private
 
   def coach_answer(question)
     if question.include? '?'
-      "Silly question, get dressed and go to work!"
+      'Silly question, get dressed and go to work!'
     else
       "I don't care, get dressed and go to work!"
     end
   end
-  end
+
   # end of class QuestionsController
 end
